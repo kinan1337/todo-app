0b414e6322b118a267bf2e8d55aff18d17533601
kinan1337 <kinanarfan@gmail.com>
2022-12-03 19:59:27 +0600

    hello,whats up

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+// const emailInput = document.querySelector(".email")
+// const passwordInput = document.querySelector(".password")
+// const login = document.querySelector(".login")
+
+
+// console.log(emailInput.value)
+
+// login.addEventListener("click", () => {
+//     const data = {
+//         email: emailInput.value,
+//         password: passwordInput.value
+//     }
+
+//     localStorage.setItem("user", JSON.stringify(data))
+// })
+
+
+// const user = JSON.parse(localStorage.getItem("user"))
+
+// console.log(user)
+
+
+// localStorage.setItem("user", "alex")
+
+// const userName = localStorage.getItem("user")
+
+// console.log(userName)\
+
+
+
+
+
